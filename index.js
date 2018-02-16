cf6b7aebcabe31005eb5e88d515dd4f4f62eab4b
Sbakken21 <sbakken21@gmail.com>
2018-02-16 14:58:27 -0600

    production scripts

--- index.js
+++ index.js
@@ -37,6 +37,17 @@ app.use(passport.session());
 // Routes
 app.use('/auth', auth);
 
+// Production Environment - use appropriate routes
+if (process.env.NODE_ENV === 'production') {
+    
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
