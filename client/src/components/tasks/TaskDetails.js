a7c025998f399c4db47f1ad2c6528a3b3c7934d5
Sbakken21 <sbakken21@gmail.com>
2018-02-20 17:18:10 -0600

    updated format of details page

--- client/src/components/tasks/TaskDetails.js
+++ client/src/components/tasks/TaskDetails.js
@@ -17,21 +17,19 @@ class TaskDetails extends Component {
         return (
             <div className="container">
                 <div className= "row">
-                {console.log(this.props.match.params.id)}
-                {console.log(task.clientName)}
                     <h2 className="center-align">{task.subject || 'N/A'}</h2>
                     <div className="card grey darken-4">
                         <div className="card-action">
-                            <h3>Client: <span>{task.clientName || 'N/A'}</span></h3> 
+                            <h5>Client: <span className="task-data">{task.clientName || 'N/A'}</span></h5>
                         </div>
                         <div className="card-action">
-                            <h5>Client Phone: {task.clientPhone || 'N/A'}</h5>
+                            <h5>Client Phone: <span className="task-data">{task.clientPhone || 'N/A'}</span></h5>
                         </div>
                         <div className="card-action">
-                            <h5>Client Email: {task.clientEmail || 'N/A'}</h5>
+                            <h5>Client Email: <span className="task-data">{task.clientEmail || 'N/A'}</span></h5>
                         </div>
                         <div className="card-action">
-                            <h5>Project Description: {task.description || 'This is a super long description. I am testing out to see if this will work in the long run and how it will render. here comes letters aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.'}</h5>
+                            <h5>Project Description: <span className="task-data">{task.description || 'N/A'}</span></h5>
                         </div>
                     </div>
                 </div>
