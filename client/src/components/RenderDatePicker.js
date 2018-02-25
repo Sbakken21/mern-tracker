f2701d01255ce4b41d1c9c55e3172131aa5f6567
Sbakken21 <sbakken21@gmail.com>
2018-02-24 22:14:13 -0600

    fixed css labels for dates

--- client/src/components/RenderDatePicker.js
+++ client/src/components/RenderDatePicker.js
@@ -6,8 +6,8 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 
 const renderDatePicker = ({input, label,  placeholder, defaultValue, meta: {touched, error} }) => (
-    <div className="datepicker">
-        <DatePicker placeholder={label} className="datepicker" {...input} dateForm="MM/DD/YYYY" selected={input.value ? moment(input.value) : null} />
+    <div className="input-field col s4 form-input">
+        <DatePicker placeholderText={label} className="datepicker" {...input} dateForm="MM/DD/YYYY" selected={input.value ? moment(input.value) : null} />
         {touched && error && <span className="red-text">{error}</span>}
     </div>
 );
