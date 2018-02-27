b88b47dd70f2769d3878ba813d24973d4e457fcd
Sbakken21 <sbakken21@gmail.com>
2018-02-26 20:20:16 -0600

    added info to landing and about pages

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,6 +5,7 @@ const About = () => {
         <div className="container">
             <h1>About</h1>
             <p>FTrak is designed using the MERN stack (MongoDB for a database, Express as a backend framework, React for the frontend, and NodeJS).</p>
+            <p>The source code can be found <a href="https://github.com/Sbakken21/mern-tracker">here.</a></p>
         </div>
     );
 };
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,6 +6,8 @@ class Landing extends Component {
             <div className="container center-align">
                 <h1>FTrack</h1>
                 <h3>FTrack is a tool for managing freelance clients and projects</h3>
+                <br />
+                <h5>Login/Register and make your job simpler</h5>
             </div>
         );
     }
